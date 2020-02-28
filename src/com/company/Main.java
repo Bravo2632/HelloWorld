a0ae1892a7956300ad89f406c8e2753a5aec7b52
Brandon Gibson <Brandon.gibson.2022@student.lhschools.org>
2020-02-28 13:40:40 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.print("My Name is ");
+        System.out.println("Brandon Gibson");
+        System.out.println("Over the summer I trained an abundance for Cross Country.");
+        System.out.println("When I wasn't running, I was eating and catching up on sleep.");
+        System.out.println("It ended up helping because I ran some of the best times for the season.");
+
+
+        //Print three more lines about what you did last summer
+
+    }
+
+}
+
+
